2c36d4b96dccc1b7a4a37b6e266833e61fab308b
vido13 <info@vido.si>
2017-11-13 00:36:48 +0100

    create, get in index funkcije za artikle

--- core/class.article.php
+++ core/class.article.php
@@ -7,6 +7,66 @@ class ARTICLE
     {
       $this->db = $DB_con;
     }
+    
+    public function create($name, $description,$price,$category,$images)
+    {
+       try
+       {
+           $statement = $this->db->prepare("INSERT INTO articles(name,description,price,category,images) VALUES(:aname,adescription,aprice,acategory,images)");
+
+           $statement->bindparam(":aname", $email);
+           $statement->bindparam(":adescription", $name);
+           $statement->bindparam(":aprice", $surname);
+           $statement->bindparam(":acategory", $address);
+           $statement->bindparam(":aimages", $images);
+           $statement->execute();
+
+           return $statement;
+       }
+       catch(PDOException $e)
+       {
+           echo $e->getMessage();
+       }
+    }
+    
+    public function get($id)
+    {
+            $sql = "SELECT * FROM articles WHERE id=$id LIMIT 1";
+            
+            try{
+                $stmt = $this->db->prepare($sql);
+                $stmt->execute();
+                $thisarticle=$stmt->fetch(PDO::FETCH_ASSOC);
+                return $thisarticle;
+                
+            }catch(PDOException $e){
+                echo $e->getMessage();
+            }
+    }
+    
+    public function index($category)
+    {
+        
+        if($category==0){
+            $sql = "SELECT * FROM articles";
+        }elseif($category==1){
+            $sql = "SELECT * FROM articles WHERE category=1";
+        }elseif($category==2){
+            $sql = "SELECT * FROM articles WHERE category=2";
+        }
+            
+            try{
+                $stmt = $this->db->prepare($sql);
+                $stmt->execute();
+                $albums=$stmt->fetchAll(PDO::FETCH_ASSOC);
+                return $articles;
+                
+            }catch(PDOException $e){
+                echo $e->getMessage();
+            }
+            
+            
+    }
 
 }
     
